281d76bf3102fca270390b9632003fd6cb0f0314
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-11-21 20:40:21 +0900

    inflearn -스프링 입문 - 코드로 배우는 스프링 부트 / 순수 jdbc를 이용한db연결 및 설정

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,23 +1,37 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 자바 코드로 직접 스프링 빈 등록하기
 // 상황에 따라 구현 클래스를 변경해야 하면 설정을 통해 스프링 빈으로 등록하기 위해 사용
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
+
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+//      return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
