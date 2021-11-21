2b10aa46ca6a0497afd2f57d07a5ee0eb852c993
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-11-21 22:06:15 +0900

    inflearn -스프링 입문 - 코드로 배우는 스프링 부트 / jpa 설정을 위한 set + 코드 수정

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,12 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 // 자바 코드로 직접 스프링 빈 등록하기
@@ -15,16 +14,17 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private final DataSource dataSource;
+    private final EntityManager em;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
+
+    public SpringConfig(DataSource dataSource,EntityManager em) {
         this.dataSource = dataSource;
+        this.em = em;
     }
 
     @Bean
     public MemberService memberService(){
-
         return new MemberService(memberRepository());
     }
 
@@ -32,6 +32,8 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
 
 //      return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//      return new JdbcMemberRepository(dataSource);
+//      return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,16 @@
 package hello.hellospring.domain;
 
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
