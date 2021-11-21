dd8d196e9e145c59cdf3dc503147d4261285c1ef
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-11-21 22:29:16 +0900

    inflearn -스프링 입문 - 코드로 배우는 스프링 부트 / aop 예

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -23,11 +23,17 @@ public class MemberService {
     }
 
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원x
-        validateDuplicateMember(member);
+        long start = System.currentTimeMillis();
 
-        memberRepository.save(member);
-        return member.getId();
+        try{
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join =" + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
