40ec499acafb78b05744d0601a3b59f3be375b8e
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-11-25 21:12:41 +0900

    feat: 스프링의 핵심 원리 -기본편 --> bulid에 lombok 추가 및 사용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,15 +4,17 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor    // final이 붙은 필드를 모아서 생성자를 자동 생성해준다.
 public class OrderServiceImpl implements OrderService{
     //  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //  private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     private final MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
 
     @Autowired  // 생성자 주입 : 생성자 호출시점에 딱 1번만 호출되는 것이 보장된다.
                 // 불변, 필수 의존관계에 사용
@@ -23,10 +25,11 @@ public class OrderServiceImpl implements OrderService{
         //  - 누군가 실수로 변경할 수 도 있고, 변경하면 안되는 메서드를 열어두는 것은 좋은 설계 방법이 아니다.
         //  - 생성자 주입은 객체를 생성할 때  딱 1번만 호출되므로 이후에 호출되는 일이 없다.
         //  - final 키워드 사용 가능 : 생성자에서 혹시라도 값이 설정되지 않는 오류를 컴파일 시점에 막아준다.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order creatOrder(Long memberId, String itemName, int itemPrice) {
