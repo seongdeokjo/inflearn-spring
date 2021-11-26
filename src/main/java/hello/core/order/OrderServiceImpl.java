b445b19a881c6de98745ffee138ca88a02c16bb3
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-11-26 16:15:11 +0900

    feat: 스프링의 핵심 원리 -기본편 --> @Autowired, @Qualifier, @Primary

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,26 +1,30 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor    // final이 붙은 필드를 모아서 생성자를 자동 생성해준다.
+//@RequiredArgsConstructor    // final이 붙은 필드를 모아서 생성자를 자동 생성해준다.
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
      // 생성자 주입 : 생성자 호출시점에 딱 1번만 호출되는 것이 보장된다.
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository,
+                                RateDiscountPolicy rateDiscountPolicy){
+//                            @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }
 
     // 불변, 필수 의존관계에 사용
     // *생성자가 딱 1개만 있으면 @Autowired를 생략해도 자동 주입 된다. 단, 스프링 빈에만 해당한다.
