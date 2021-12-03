7444d03431a2f44a19dc37c6c10b30e281689f66
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-12-03 16:22:10 +0900

    feat: inflearn- springmvc1 - 뷰 리졸버

--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -7,11 +7,15 @@ import org.springframework.web.servlet.mvc.Controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
+
+
 @Component("/springmvc/old-controller")
 public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form");
     }
 }
