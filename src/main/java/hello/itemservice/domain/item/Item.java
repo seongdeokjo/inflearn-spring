a6a3761784ea86a6c60862d73e432239716850c8
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-12-09 20:29:35 +0900

    feat: inflearn - springmvc2 - Bean Validation - 시작

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,25 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;   // 하이버네이트 validator 구현체를 사용할 때만 제공되는 검증 기능
+
+import javax.validation.constraints.Max;        // 특정 구현에 관계없이 제공되는 표준 인터페이스
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
     private Long id;
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max=1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
