4126ad0ebe8147b209f7938d6da7bed6e49f41d3
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-12-09 21:07:26 +0900

    feat: inflearn - springmvc2 - Bean Validation - 오브젝트 오류

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,15 +2,18 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;   // 하이버네이트 validator 구현체를 사용할 때만 제공되는 검증 기능
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;        // 특정 구현에 관계없이 제공되는 표준 인터페이스
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang= "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요.")
 public class Item {
 
     private Long id;
+
     @NotBlank(message = "공백x")
     private String itemName;
 
