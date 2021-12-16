723f99bc99bb3b77d02cef6ffd8129d05119a4da
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-12-16 12:03:08 +0900

    feat: inflearn - spring advanced - 구체 클래스 기반 프록시 - 예제2 / 다형성 이용

--- src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.concreteproxy;
 
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreteproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -13,5 +14,11 @@ public class ConcreteProxyTest {
         client.execute();
     }
 
-
+    @Test
+    void addProxy(){
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
+        client.execute();
+    }
 }
