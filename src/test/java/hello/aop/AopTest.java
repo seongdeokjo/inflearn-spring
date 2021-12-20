9941d58da0c2e16c06bd22c4c79af12b74496a9d
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-12-20 20:27:07 +0900

    feat: inflearn - springadvanced - 스프링 aop 구현4 - 포인트컷 참조

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -5,6 +5,7 @@ import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV2;
 import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Poincut;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -18,7 +19,8 @@ import static org.assertj.core.api.Assertions.*;
 @SpringBootTest
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Poincut.class)
 public class AopTest {
 
     @Autowired
