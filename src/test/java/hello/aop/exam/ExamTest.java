67e7c02138ed0b9f65ce74d2024c015024c58020
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-12-21 20:42:39 +0900

    feat: inflearn - springadvanced - 스프링 aop - 실전예제 - 재시도 aop

--- src/test/java/hello/aop/exam/ExamTest.java
+++ src/test/java/hello/aop/exam/ExamTest.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.aop.RetryAspect;
 import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Slf4j
-@Import(TraceAspect.class)
+@Import({TraceAspect.class, RetryAspect.class})
 @SpringBootTest
 public class ExamTest {
 
@@ -16,9 +17,9 @@ public class ExamTest {
     ExamService examService;
 
     @Test
-    void test(){
-        for(int i = 0; i < 5; i++){
-            log.info("client request i={}",i);
+    void test() {
+        for (int i = 0; i < 5; i++) {
+            log.info("client request i={}", i);
             examService.request("data" + i);
         }
     }
